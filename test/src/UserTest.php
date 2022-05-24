854b8b25d981b24ab424993b76f79a353c0c64f6
Roland Rusch <roland@rusch.lu>
2022-05-25 00:31:33 +0200

    #1 Initial commit

---
+++ test/src/UserTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ruga\Party\Test;
+
+
+/**
+ * @author Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ */
+class UserTest extends \Ruga\Party\Test\PHPUnit\AbstractTestSetUp
+{
+    public function testCanFindPartyOfUser(): void
+    {
+        /** @var \Ruga\User\User $user */
+        $user = $this->getAdapter()->rowFactory('4@UserTable');
+        echo "User: {$user->idname}" . PHP_EOL;
+        $this->assertInstanceOf(\Ruga\User\User::class, $user);
+        
+        /** @var \Ruga\Party\PartyTable $partyTable */
+        $partyTable = $this->getAdapter()->tableFactory(\Ruga\Party\PartyTable::class);
+        
+        /** @var \Ruga\Party\Party $party */
+        $party = $partyTable->findByUser($user)->current();
+        echo "Party: {$party->idname}" . PHP_EOL;
+        $this->assertInstanceOf(\Ruga\Party\Party::class, $party);
+    }
+    
+    
+    
+    public function testCanFindTenantOfUser(): void
+    {
+        /** @var \Ruga\User\User $user */
+        $user = $this->getAdapter()->rowFactory('4@UserTable');
+        echo "User: {$user->idname}" . PHP_EOL;
+        $this->assertInstanceOf(\Ruga\User\User::class, $user);
+        
+        /** @var \Ruga\Party\PartyTable $partyTable */
+        $partyTable = $this->getAdapter()->tableFactory(\Ruga\Party\PartyTable::class);
+        
+        /** @var \Ruga\Party\Party $userParty */
+        $userParty = $partyTable->findByUser($user)->current();
+        echo "User Party: {$userParty->idname}" . PHP_EOL;
+        $this->assertInstanceOf(\Ruga\Party\Party::class, $userParty);
+        
+        /** @var \Ruga\Party\Party $tenantParty */
+        $tenantParty = $userParty->findRelatedParties(
+            \Ruga\Party\Relationship\PartyRelationshipType::REPRESENTATIVE()
+        )->current();
+        echo "Tenant Party: {$tenantParty->idname}" . PHP_EOL;
+        $this->assertInstanceOf(\Ruga\Party\Party::class, $tenantParty);
+        
+        /** @var \Ruga\Party\Tenant\Tenant $tenant */
+        $tenant = (new \Ruga\Party\Tenant\TenantTable($this->getAdapter()))->select(['Party_id' => $tenantParty->PK]
+        )->current();
+        echo "Tenant: {$tenant->idname}" . PHP_EOL;
+        $this->assertInstanceOf(\Ruga\Party\Tenant\Tenant::class, $tenant);
+    }
+    
+    
+    
+    public function testCanFindTenantsOfUser(): void
+    {
+        /** @var \Ruga\User\User $user */
+        $user = $this->getAdapter()->rowFactory('4@UserTable');
+        echo "User: {$user->idname}" . PHP_EOL;
+        $this->assertInstanceOf(\Ruga\User\User::class, $user);
+        
+        /** @var \Ruga\Party\Tenant\TenantTable $tenantTable */
+        $tenantTable = $this->getAdapter()->tableFactory(\Ruga\Party\Tenant\TenantTable::class);
+        $tenants = $tenantTable->findByUser($user);
+        /** @var \Ruga\Party\Tenant\Tenant $tenant */
+        foreach ($tenants as $tenant) {
+            echo "Tenant: {$tenant->idname}" . PHP_EOL;
+            $this->assertInstanceOf(\Ruga\Party\Tenant\Tenant::class, $tenant);
+        }
+    }
+    
+}
