f509b6c814a2f32afbc030b49899a5278de1f55c
Roland Rusch <roland@rusch.lu>
2023-09-25 09:24:50 +0200

    #10 Change License to AGPL-3.0-only

--- src/Tenant/Container/TenantTableFactory.php
+++ src/Tenant/Container/TenantTableFactory.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 declare(strict_types=1);
 
@@ -8,11 +12,12 @@ use Psr\Container\ContainerInterface;
 use Ruga\Db\Adapter\Adapter;
 use Ruga\Party\Customer\CustomerTable;
 use Ruga\Party\Tenant\AbstractTenantTable;
+use Ruga\Party\Tenant\TenantTable;
 
 class TenantTableFactory
 {
     public function __invoke(ContainerInterface $container): AbstractTenantTable
     {
-        return new CustomerTable($container->get(Adapter::class));
+        return new TenantTable($container->get(Adapter::class));
     }
 }
